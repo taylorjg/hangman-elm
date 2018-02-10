cc78d691e8320ef32da64463238e6dafcd840bee
Jonathan Taylor <jont@cakesolutions.net>
2018-02-10 11:26:29 +0000

    Force a build

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -12,7 +12,7 @@ all =
     describe "A Test Suite"
         [ test "Addition" <|
             \_ ->
-                Expect.equal 10 (3 + 7)
+                Expect.equal 11 (4 + 7)
         , test "String.left" <|
             \_ ->
                 Expect.equal "a" (String.left 1 "abcdefg")
